a7ab06fd9d9cbb25d8eddf293018c1bc3fe4463d
rishabh-malhotra <rishabh11.rm@gmail.com>
2017-04-02 00:13:25 +0530

    second commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage</title>
+	<style type="text/css">
+		a{
+			text-decoration: none;
+		}
+		a:hover{
+			color: #b59bc6;
+		}
+
+
+	</style>
+</head>
+<body>
+	<h1>WELCOME TO THE SIMPLE LOGIN SYSTEM</h1>
+	Please select one of the option
+	<ul>
+		<li><a href="login.php">LOGIN</a></li>
+		<li><a href="register.php">REGISTER</a></li>
+	</ul>
+</body>
+</html>
