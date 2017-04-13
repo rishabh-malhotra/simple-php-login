eb4e4ed2e066a460b99985b492de1cf8f5fe67f2
rishabh-malhotra <rishabh11.rm@gmail.com>
2017-04-13 19:45:07 +0530

    made some changes in registeration page

--- register.php
+++ register.php
@@ -1,7 +1,6 @@
 <?php
 
-
-$servername = "localhost";
+	$servername = "localhost";
 	$username = "root";
 	$password = "";
 	$db="login";
@@ -11,15 +10,23 @@ $servername = "localhost";
 
 	// Check connection
 	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
+		die("Connection failed: " . $conn->connect_error);
 	}
 
-
-$name=$_POST['name'];
-$username=$_POST['username'];
-$password=$_POST['password'];
-mysqli_query($conn,"INSERT INTO `user`(`name`, `username`, `password`) VALUES ('$name','$username','$password')");
-
+	if($_POST['register']){
+	$name=$_POST['name'];
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	$result=mysqli_query($conn,"INSERT INTO `user`(`name`, `username`, `password`) VALUES ('$name','$username','$password')");
+		//echo "REGISTRATION Successful";
+		//else
+		//echo "Can't Register try again";	
+	if($result){
+		echo "REGISTRATION Successful";
+	}
+	else
+		echo "Can't Register try again";
+}
 ?>
 
 
@@ -33,13 +40,13 @@ mysqli_query($conn,"INSERT INTO `user`(`name`, `username`, `password`) VALUES ('
 	<h1>REGISTRATION PAGE</h1>
 	<form method="POST" action="register.php">
 		<table>
-		<tr><td>NAME: </td><td><input type="text" name="name" placeholder="Enter your name"></td></tr>
+			<tr><td>NAME: </td><td><input type="text" name="name" placeholder="Enter your name"></td></tr>
 			<tr><td>USERNAME: </td><td><input type="text" name="username" placeholder="Choose your username"></td></tr>
 			<tr><td>PASSWORD: </td><td><input type="password" name="password" placeholder="Enter your selected password"></td></tr>
-			<tr><td><input type="submit" value="REGISTER"></td></tr>
+			<tr><td><input type="submit" name="register" value="REGISTER"></td></tr>
 		</table>
 		<h4>ALREADY REGISTERED..</h4>
-		go to login page..<a href="login.php">CLick here</button>
+		go to login page..<a href="index.php">CLick here</a></button>
 	</form>
 </body>
 </html>
