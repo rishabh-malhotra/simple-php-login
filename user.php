1a9b65a23feec0f1e6478d9555ca7bb59c949472
rishabh-malhotra <rishabh11.rm@gmail.com>
2017-06-06 19:23:33 +0530

    logout.php added

--- user.php
+++ user.php
@@ -1,10 +1,16 @@
 <?php
 session_start();
 
+if (isset($_SESSION['username'])){
 $username=$_SESSION['username'];
 $name=$_SESSION['name'];
 echo "hello $name with username : $username";
 echo " <p><b>WELCOME you are logged in!!</b>";
+}
+else{
+	header('Location:index.php');
+	die();
+}
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +20,7 @@ echo " <p><b>WELCOME you are logged in!!</b>";
 </head>
 <body>
 <form action="logout.php">
-	<input type="submit" name="submit" value="LOGOUT">
+	<input type="submit" name="logout" value="LOGOUT">
 </form>
 </body>
 </html>
